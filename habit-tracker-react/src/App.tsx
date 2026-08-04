2fa63e3b26f3f54957da111fe8429d3264cf1441
devseravali <devseravali@gmail.com>
2026-08-04 14:28:29 -0300

    feat: reorganiza estrutura do componente App para incluir Sidebar e layout

--- habit-tracker-react/src/App.tsx
+++ habit-tracker-react/src/App.tsx
@@ -4,27 +4,33 @@ import { Habitos } from './pages/Habitos/Habitos';
 import { Configuracoes } from './pages/Configuracoes/Configuracoes';
 import { MetasSemanais } from './pages/MetasSemanais/MetasSemanais';
 import { Estatisticas } from './pages/Estatisticas/Estatisticas';
+import { Sidebar } from './components/Sidebar/Sidedar';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/overview"
-          element={<PaginaInicial pageTitle="Overview" />}
-        />
-        <Route
-          path="/habitos"
-          element={<Habitos />}
-        />
-        <Route path="/estatisticas" element={<Estatisticas />} />
-        <Route path="/metas-semanais" element={<MetasSemanais />} />
-        <Route
-          path="/configuracoes"
-          element={<Configuracoes />}
-        />
-        <Route path="/" element={<Navigate to="/overview" replace />} />
-        <Route path="*" element={<Navigate to="/overview" replace />} />
-      </Routes>
+      <div className="layout-grid">
+        <Sidebar />
+        <div className="app-shell">
+          <Routes>
+            <Route
+              path="/overview"
+              element={<PaginaInicial pageTitle="Overview" />}
+            />
+            <Route
+              path="/habitos"
+              element={<Habitos />}
+            />
+            <Route path="/estatisticas" element={<Estatisticas />} />
+            <Route path="/metas-semanais" element={<MetasSemanais />} />
+            <Route
+              path="/configuracoes"
+              element={<Configuracoes />}
+            />
+            <Route path="/" element={<Navigate to="/overview" replace />} />
+            <Route path="*" element={<Navigate to="/overview" replace />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
