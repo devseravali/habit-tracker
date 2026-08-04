edc7490d8342cd283c8ed2cafc8407b064b2bfd8
devseravali <devseravali@gmail.com>
2026-08-04 14:27:27 -0300

    feat: adiciona componentes Footer e Sidebar com estrutura básica

---
+++ habit-tracker-react/src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>© 2024 Habit Tracker. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
---
+++ habit-tracker-react/src/components/Sidebar/Sidedar.tsx
@@ -0,0 +1,25 @@
+import { Header } from '../Header/Header';
+
+export const Sidebar: React.FC = () => {
+    return (
+        <>
+        <aside className="sidebar">
+            <div className="brand">Habit Tracker</div>
+            <div className="brand-sub">Protocol: Habit-Track-v1</div>
+
+               <Header />
+
+            <div className="sidebar-footer">
+                <button className="upgrade" id="upgrade-btn">
+                    <span className="material-symbols-outlined">star</span>
+                    Upgrade
+                </button>
+
+                <a id="support-link" href="#">Suporte</a>
+
+                <button className="danger">Sair</button>
+            </div>
+        </aside>
+    </>
+    );
+};
