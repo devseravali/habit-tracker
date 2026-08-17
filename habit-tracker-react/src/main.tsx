d7f68f00d0a413323a4243648d4ecdcb73c9e331
devseravali <devseravali@gmail.com>
2026-08-17 15:26:30 -0300

    feat: remoção de designs

--- habit-tracker-react/src/main.tsx
+++ habit-tracker-react/src/main.tsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './styles/reset.css';
 import './styles/theme.css';
-import './styles/style.css';
-import './styles/responsive.css';
-
+import './styles/reset.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
